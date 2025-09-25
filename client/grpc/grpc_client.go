8f80973c94911f20eb7d12cac5dc52710fa140f7
RenSafary <102967188+RenSafary@users.noreply.github.com>
2025-09-25 20:24:46 +0500

    sign-up grpc service

---
+++ client/grpc/grpc_client.go
@@ -0,0 +1,36 @@
+package grpc
+
+import (
+	pb "AuthService/proto"
+	"context"
+	"log"
+	"time"
+
+	"google.golang.org/grpc"
+)
+
+func SignUpGRPC(email, username string, password []byte) (bool, string) {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Println(err)
+		return false, "Couldn't connect to gRPC server"
+	}
+	defer conn.Close()
+
+	client := pb.NewAuthServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	resp, err := client.SignUp(ctx, &pb.SignUpRequest{
+		Email:    email,
+		Username: username,
+		Password: string(password),
+	})
+	if err != nil {
+		log.Println(err)
+		return false, "Couldn't request 'SignUp' method"
+	}
+
+	return resp.Status, resp.Token
+}
