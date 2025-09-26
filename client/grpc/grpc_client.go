809335068e6cfb05371a032ef30254bacb55e226
RenSafary <102967188+RenSafary@users.noreply.github.com>
2025-09-26 23:18:46 +0500

    grpc sign-in client

--- client/grpc/grpc_client.go
+++ client/grpc/grpc_client.go
@@ -34,3 +34,28 @@ func SignUpGRPC(email, username string, password []byte) (bool, string) {
 
 	return resp.Status, resp.Token
 }
+
+func SignInGRPC(username, password string) (bool, string) {
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Println(err)
+		return false, "Couldn't connecto to gRPC server"
+	}
+	defer conn.Close()
+
+	client := pb.NewAuthServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	resp, err := client.SignIn(ctx, &pb.SignInRequest{
+		Username: username,
+		Password: password,
+	})
+	if err != nil {
+		log.Println(err)
+		return false, "Couldn't request 'SignIn' method"
+	}
+
+	return resp.Status, resp.Token
+}
