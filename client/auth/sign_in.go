550130c083745a6b839aa9b08425b5d9f1ee5c34
RenSafary <102967188+RenSafary@users.noreply.github.com>
2025-09-26 23:37:05 +0500

    using grpc-client

--- client/auth/sign_in.go
+++ client/auth/sign_in.go
@@ -1,6 +1,8 @@
 package auth
 
 import (
+	"AuthService/client/grpc"
+	encryption "AuthService/client/utils"
 	"log"
 	"net/http"
 	"text/template"
@@ -22,4 +24,29 @@ func GetUserDataSignIn(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Methods", "POST")
 	w.Header().Set("Content-Type", "text/html")
 
+	err := r.ParseForm()
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	hash_pass, err := encryption.EncryptPass(password)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	response, token := grpc.SignInGRPC(username, string(hash_pass))
+
+	tmpl, err := template.ParseFiles("templates/response.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data := &ResponseData{Response: response, Token: token}
+
+	tmpl.Execute(w, data)
 }
