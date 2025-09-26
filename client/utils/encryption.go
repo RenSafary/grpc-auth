f10dab5539aca94a75ac1d96c3945e95a802be60
RenSafary <102967188+RenSafary@users.noreply.github.com>
2025-09-26 23:14:41 +0500

    factorization

---
+++ client/utils/encryption.go
@@ -0,0 +1,11 @@
+package encryption
+
+import "golang.org/x/crypto/bcrypt"
+
+func EncryptPass(password string) ([]byte, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+	return hash, nil
+}
